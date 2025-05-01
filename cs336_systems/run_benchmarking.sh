ae1cf6a4d3f432610e4c05f89feb41c66a19ff90
Aditya Tadimeti <adityatadimeti@gmail.com>
2025-05-01 14:26:52 -0700

    output amp path for benchmarking

--- cs336_systems/run_benchmarking.sh
+++ cs336_systems/run_benchmarking.sh
@@ -7,14 +7,16 @@
 #SBATCH --gpus=1
 set -euo pipefail
 
+mkdir -p amp
+
+
 # 1) Define “sweeps”:
 declare -a ctxs=(128 256 512 1024)
 # key = d_model, values = d_ff, num_layers, num_heads
 declare -A d_ff=(   [768]=3072   [1024]=4096   [1280]=5120   [1600]=6400   [2560]=10240 )
 declare -A layers=( [768]=12     [1024]=24     [1280]=36     [1600]=48     [2560]=32    )
 declare -A heads=(  [768]=12     [1024]=16     [1280]=20     [1600]=25     [2560]=32    )
-#declare -a d_models=(768 1024 1280 1600 2560)
-declare -a d_models=(2560)
+declare -a d_models=(768 1024 1280 1600 2560)
 
 # 2) Loop and run nsys with a param-derived output name:
 for d in "${d_models[@]}"; do
@@ -22,7 +24,7 @@ for d in "${d_models[@]}"; do
     f=${d_ff[$d]}
     nl=${layers[$d]}
     nh=${heads[$d]}
-    out="nsys_ctx${ctx}_d${d}_ff${f}_l${nl}_h${nh}.qdrep"
+    out="amp/nsys_ctx${ctx}_d${d}_ff${f}_l${nl}_h${nh}.qdrep"
 
     # skip if we've already profiled this setting
     if [[ -f "${out}" ]]; then
